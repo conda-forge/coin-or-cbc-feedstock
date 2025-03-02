e36b7e8db1592b124b512d7984a79af1ed03d9f3
Ted Ralphs <ted@lehigh.edu>
2025-03-02 13:22:28 -0500

    Add OsiClp as dependency

--- recipe/build_package.sh
+++ recipe/build_package.sh
@@ -9,14 +9,8 @@ else
 fi
 
 if [[ "${target_platform}" == win-* ]]; then
-  #COINUTILS_LIB=( --with-coinutils-lib='${LIBRARY_PREFIX}/lib/mkl_intel_ilp64.lib ${LIBRARY_PREFIX}/lib/mkl_sequential.lib ${LIBRARY_PREFIX}/lib/mkl_core.lib ${LIBRARY_PREFIX}/lib/libCoinUtils.lib' )
-  #COINUTILS_INC=( --with-coinutils-incdir='${LIBRARY_PREFIX_COIN}' )
-  #OSI_LIB=( --with-osi-lib='${LIBRARY_PREFIX}/lib/libOsi.lib' )
-  #OSI_INC=( --with-osi-incdir='${LIBRARY_PREFIX_COIN}' )
-  CLP_LIB=( --with-clp-lib='${LIBRARY_PREFIX}/lib/libClp.lib' )
+  CLP_LIB=( --with-clp-lib='${LIBRARY_PREFIX}/lib/libClp.lib ${LIBRARY_PREFIX}/lib/libOsiClp.lib' )
   CGL_INC=( --with-clp-incdir='${LIBRARY_PREFIX_COIN}' )
-  #CGL_LIB=( --with-cgl-lib='${LIBRARY_PREFIX}/lib/libCgl.lib' )
-  #CLP_INC=( --with-cgl-incdir='${LIBRARY_PREFIX_COIN}' )
   COINDEPEND_LIB=( --with-coindepend-lib='${LIBRARY_PREFIX}/lib/mkl_intel_ilp64.lib ${LIBRARY_PREFIX}/lib/mkl_sequential.lib ${LIBRARY_PREFIX}/lib/mkl_core.lib ${LIBRARY_PREFIX}/lib/libCoinUtils.lib ${LIBRARY_PREFIX}/lib/libOsi.lib ${LIBRARY_PREFIX}/lib/libCgl.lib' )
   COINDEPEND_INC=( --with-coindepend-incdir='${LIBRARY_PREFIX_COIN}' )
   EXTRA_FLAGS=( --enable-msvc ) 
@@ -24,14 +18,8 @@ else
   # Get an updated config.sub and config.guess (for mac arm and lnx aarch64)
   cp $BUILD_PREFIX/share/gnuconfig/config.* ./Cbc 
   cp $BUILD_PREFIX/share/gnuconfig/config.* .
-  #COINUTLS_LIB=()
-  #COINUTILS_INC=()
-  #OSI_LIB=()
-  #OSI_INC=()
   CLP_LIB=()
   CLP_INC=()
-  #CGL_LIB=()
-  #CGL_INC=()
   COINDEPEND_LIB=()
   COINDEPEND_INC=()
   EXTRA_FLAGS=()
